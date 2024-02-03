1b8c53d7fab558c20ce3b0cadeee5f3b7e43e7ce
Paradeevic <152689762+Paradeevic@users.noreply.github.com>
2024-02-03 13:50:03 +0300

    Add files via upload

---
+++ dodle-jump-main/scripts/constants.py
@@ -0,0 +1,3 @@
+import pygame
+display_size = (400,720)   
+CreatePlatform_Event = pygame.USERVENT + 1
---
+++ dodle-jump-main/scripts/game.py
@@ -0,0 +1,47 @@
+import pygame
+import os
+from scripts.platform_generator import Platform_generator
+from scripts.constants import display_size
+from scripts.player import Player
+from scripts.platform import Platform
+from scripts.functions import load_image
+from scripts.sprite import Sprite
+class Game:
+    def __init__(self):
+        self.background = load_image("assets","images","background.png")
+        self.platform_generator = Platform_generator(200)
+        self.player = Player((200,200),load_image("assets","images",'player.png'),5,20,0.65)
+        self.offset_y = 0
+        self.platforms = list()
+            
+                            
+           
+    
+    def render(self,surface: pygame.Surface):
+        surface.blit(self.background,(0,0))
+        for platform in self.platforms:
+            platform.render(surface,self.offset_y)
+        self.player.render(surface,self.offset_y)
+    
+    def handle_key_down_event(self,key):
+        if key == pygame.K_a:
+            self.player.is_walking_left = True
+        
+        elif key == pygame.K_d:
+            self.player.is_walking_right = True   
+    def handle_key_up_event(self,key):
+        if  key == pygame.K_a:
+            self.player.is_walking_left = False
+        
+        elif key == pygame.K_d:
+            self.player.is_walking_right = False
+    def update(self):
+        self.player.update()
+        for platform in self.platforms:
+            if self.player.collide_sprite(platform):
+                self.player.on_platform = True
+        if self.player.rect.bottom - self.offset_y <display_size[1]/3:
+             self.offset_y = self.player.rect.bottom - display_size[1]/3
+        self.platform_generator.update(self.offset_y, self.platforms)
+        
+            
